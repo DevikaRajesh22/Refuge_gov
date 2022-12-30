820bd4348c7d5d1be5c9f62060b2696046831d3c
Akshaya M V <akshaya1672@gmail.com>
2022-12-30 12:58:32 +0530

    set forgot password,allignment,color

--- lib/ui/screens/login_page.dart
+++ lib/ui/screens/login_page.dart
@@ -13,41 +13,70 @@ class LoginPage extends StatelessWidget {
               height: 800,
               width: 400,
               // ignore: prefer_const_literals_to_create_immutables
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(colors: [
-                  Color.fromARGB(255, 56, 69, 184),
-                  Color.fromARGB(95, 45, 58, 227),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 2, 9, 98),
+                  Color.fromARGB(255, 0, 108, 174),
                 ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
               ),
-              child: const Text("logo"),
-            ),
-            Expanded(
               child: Column(
                 children: const [
+                  Text("logo"),
                   Text(
+                    "We are for you",
                     textAlign: TextAlign.center,
-                    'Welcome Back, Admin User',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 69, 30, 197),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0,
+                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(250),
+                child: Column(
+                  children: [
+                    const Text(
+                      textAlign: TextAlign.center,
+                      'Welcome Back, Admin User',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 69, 30, 197),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                      ),
                     ),
-                  ),
-                  Text('Sign in by entering the information below'),
-                  TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email Address',
-                        hintText: 'mail@user.com'),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 10)),
-                  TextField(
-                      obscureText: true,
+                    const Text('Sign in by entering the information below'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const TextField(
                       decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: 'Password',
-                          hintText: 'Enter password')),
-                ],
+                          labelText: 'Email Address',
+                          hintText: 'mail@user.com'),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 10)),
+                    const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            suffixIcon: Icon(Icons.visibility),
+                            labelText: 'Password',
+                            hintText: 'Enter password')),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "Forgot password?",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 69, 30, 197)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ],
