b91e440b324c8a6880db9c8a2225e6fc9942ef63
DevikaRajesh22 <devikaraj699@gmail.com>
2023-02-24 08:35:57 +0530

    NGO screen completed

--- lib/ui/screens/ngo_screen.dart
+++ lib/ui/screens/ngo_screen.dart
@@ -113,15 +113,42 @@ class NgoScreen extends StatelessWidget {
                             height: 40,
                           ),
                           Container(
-                            height: 150,
+                            height: 200,
                             width: 400,
                             color: const Color.fromARGB(200, 146, 197, 224),
                             child: Text(
-                              'Founding year : 2010\nOwnership: Name\nTotal volunteers: 67\nTransport vehicles: 18\nFood Services: Yes\nAddress: xyz Building place pin',
+                              '\n  Founding year : 2010\n  Ownership: Name\n  Total volunteers: 67\n  Transport vehicles: 18\n  Food Services: Yes\n  Address: xyz Building place pin',
                               style: GoogleFonts.inriaSans(
-                                fontSize: 15,
-                              ),
+                                  fontSize: 18,
+                                  color: const Color.fromARGB(255, 3, 4, 94)),
                             ),
+                          ),
+                          const SizedBox(
+                            height: 70,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Accept',
+                                style: GoogleFonts.inriaSans(
+                                  color: const Color.fromARGB(255, 3, 4, 94),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 60,
+                              ),
+                              Text(
+                                'Reject',
+                                style: GoogleFonts.inriaSans(
+                                  color: const Color.fromARGB(180, 3, 4, 94),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
                           const SizedBox(
                             height: 30,
@@ -147,13 +174,90 @@ class NgoScreen extends StatelessWidget {
                           horizontal: 20, vertical: 15),
                       child: Column(
                         children: [
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Column(
-                            children: const [
-                              //type
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/images/people3.png',
+                                height: 70,
+                                width: 70,
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'Island Development Organization',
+                                    style: GoogleFonts.inriaSans(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color:
+                                          const Color.fromARGB(255, 3, 4, 94),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Made for the people',
+                                    style: GoogleFonts.inriaSans(
+                                      fontSize: 15,
+                                      color:
+                                          const Color.fromARGB(255, 3, 4, 94),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Requested 15 minutes ago...',
+                                    style: GoogleFonts.inriaSans(
+                                        fontSize: 13,
+                                        color:
+                                            const Color.fromARGB(150, 0, 0, 0)),
+                                  ),
+                                ],
+                              ),
                             ],
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Container(
+                            height: 200,
+                            width: 400,
+                            color: const Color.fromARGB(200, 146, 197, 224),
+                            child: Text(
+                              '\n  Founding year : 2009\n  Ownership: Name\n  Total volunteers: 89\n  Transport vehicles: 16\n  Food Services: Yes\n  Shelter facilities: Yes\n  Addrerss: abc building place pin',
+                              style: GoogleFonts.inriaSans(
+                                  fontSize: 18,
+                                  color: const Color.fromARGB(255, 3, 4, 94)),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 70,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Accept',
+                                style: GoogleFonts.inriaSans(
+                                  color: const Color.fromARGB(255, 3, 4, 94),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 60,
+                              ),
+                              Text(
+                                'Reject',
+                                style: GoogleFonts.inriaSans(
+                                  color: const Color.fromARGB(180, 3, 4, 94),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
                           ),
                         ],
                       ),
